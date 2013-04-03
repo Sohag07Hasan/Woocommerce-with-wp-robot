33ddf3f17687334bf41d687d4ff61ca4275e517e
Mahibul Hasan Sohag <hyde.sohag@gmail.com>
2013-04-03 10:17:31 +0600

    working

--- main.php
+++ main.php
@@ -4,7 +4,8 @@
  * plugin name: WpRObot with Woocommerce
  * author: Mahibul Hasan
  * description: Automatically runs a cron to convert amazon post into wocoommerce products. Dynamically fetched the prices, description, features from amazon. The shop will work exactly woocommerce enviornment. While payment options is to handled, it will take the visitors to the amazon with referrel id
- * 
+ * plugin uri: http://www.bouncerseat.net/
+ * author uri: http://sohag07hasan.elance.com
  * */
 
 define("WPROBOTWOOCOMMERCE_DIR", dirname(__FILE__));
